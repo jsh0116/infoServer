99277eb1953944413713a05619808be1f4cbcf2e
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-20 17:36:15 +0900

    Initial commit

---
+++ src/test/java/com/example/infoServer/InfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
